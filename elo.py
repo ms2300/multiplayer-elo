ee5e0899e8ff513af336589876abd4cd89ed922b
ms2300 <matt.sewall@gmail.com>
2016-11-24 23:02:23 -0700

    removed bad import

--- elo.py
+++ elo.py
@@ -3,7 +3,6 @@
 # Copyright (c) 2016 by Matt Sewall.
 # All rights reserved.
 import math
-from elo_classes import *
 
 # Background information - https://en.wikipedia.org/wiki/Elo_rating_system
 # Based on this equation - http://elo-norsak.rhcloud.com/index.php
@@ -20,14 +19,13 @@ _LOSS = 0.0
 
 def calculateElo(players):
     N = len(players)
-    # compare every head to head matchup in given competition
+    # changing K multiplier depending on the size of the competition
+    if N > _MAXSIZE:
+        K = 1.2
+    else:
+        k_slope = 3 * N / 175
+        K = _KMAX - k_slope
     for i in players:
-        # changing K multiplier depending on the size of the competition
-        if N > _MAXSIZE:
-            K = 1.2
-        else:
-            k_slope = 3 * N / 175
-            K = _KMAX - k_slope
         # Players with higher elo receive lower K values for stability
         multi = 1
         if i.elo > _MINBUFFEREDELO and i.elo < _MAXBUFFEREDELO:
@@ -35,7 +33,7 @@ def calculateElo(players):
         K = multi * K
         for j in players:
             if i is not j:
-                # S is the player's actual score - 1 for win - 0 for loss
+                # S is the player's actual score - (1 for win) - (0 for loss)
                 if i.place < j.place:
                     S = _WIN
                 else:
